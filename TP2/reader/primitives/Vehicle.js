1cde4d4167f0f1adbda34859a014a8aeee7d7629
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2015-11-15 18:12:24 +0000

    Melhorei o aspeto do corpo do barco. :boat:
    Vou fazer o chão do barco

--- TP2/reader/primitives/Vehicle.js
+++ TP2/reader/primitives/Vehicle.js
@@ -13,16 +13,13 @@ Vehicle.prototype.constructor = Vehicle;
 Vehicle.prototype.construi_partes = function()
 {
 	//Corpo
-	this.Array_Partes["corpo"] = new SpherePrimitive(this.scene,20,20,0.5);
 	this.Array_Texturas["corpo"] = new CGFtexture(this.scene, "primitives/assets/Boards.jpg");	
-	
-	//Corpo
-		this.Array_Partes["barco"] = new Patch(this.scene, 3,4,4,
+	this.Array_Partes["corpo"] = new Patch(this.scene, 3,4,4,
 		[
-		[-1,0,1],[-1,-1,0],[-1,0,-1],[-1,0,-1],
-		[0,0,1],[0,-1,0],[0,0,-1],[-1,0,-1],
-		[1,0,1],[1,-1,0],[1,0,-1],[-1,0,-1],
-		[1,0,1],[1,-1,0],[1,0,-1],[-1,0,-1]
+		[0,0.5,2],[0,0.5,2],[0,0.5,2],[0,0.5,2],
+		[-1,0,1],[0,-2,1],[0,-2,1],[1,0,1],
+		[-1,0,0],[0,-2,0],[0,-2,0],[1,0,0],
+		[-1,0,-1],[0,0,-1.5],[0,0,-1.5],[1,0,-1]
 		]);
 }
 
@@ -33,7 +30,7 @@ Vehicle.prototype.display = function()
 	
 	//Corpo
 	this.Array_Texturas["corpo"].bind();
-	this.Array_Partes["barco"].display();
+	this.Array_Partes["corpo"].display();
 	
 	this.scene.popMatrix();
 }

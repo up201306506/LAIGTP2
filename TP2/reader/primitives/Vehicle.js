497b4fa06393e1e0cfb86e0f4e04ce52670cf8ac
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2015-11-15 17:58:29 +0000

    O veiculo vai ser um barco.  :boat:
    O patch está todo mal mas dá para perceber a idea

--- TP2/reader/primitives/Vehicle.js
+++ TP2/reader/primitives/Vehicle.js
@@ -10,22 +10,30 @@ function Vehicle(scene){
 Vehicle.prototype = Object.create(CGFobject.prototype);
 Vehicle.prototype.constructor = Vehicle;
 
-Vehicle.prototype.construi_partes = function(timestart)
+Vehicle.prototype.construi_partes = function()
 {
 	//Corpo
 	this.Array_Partes["corpo"] = new SpherePrimitive(this.scene,20,20,0.5);
-	this.Array_Texturas["corpo"] = new CGFtexture(this.scene, "primitives/assets/veiculo.jpg");
+	this.Array_Texturas["corpo"] = new CGFtexture(this.scene, "primitives/assets/Boards.jpg");	
 	
+	//Corpo
+		this.Array_Partes["barco"] = new Patch(this.scene, 3,4,4,
+		[
+		[-1,0,1],[-1,-1,0],[-1,0,-1],[-1,0,-1],
+		[0,0,1],[0,-1,0],[0,0,-1],[-1,0,-1],
+		[1,0,1],[1,-1,0],[1,0,-1],[-1,0,-1],
+		[1,0,1],[1,-1,0],[1,0,-1],[-1,0,-1]
+		]);
 }
 
 
-Vehicle.prototype.display = function(timestart)
+Vehicle.prototype.display = function()
 {
 	this.scene.pushMatrix();
 	
 	//Corpo
 	this.Array_Texturas["corpo"].bind();
-	this.Array_Partes["corpo"].display();
+	this.Array_Partes["barco"].display();
 	
 	this.scene.popMatrix();
 }

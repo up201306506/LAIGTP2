d50b94615f8f0829b2974e3091a8bba081c69e8b
Pedro Carvalho <up201306506@gcloud.fe.up.pt>
2015-11-15 05:03:10 +0000

    Isto dos shaders dá cá um nó na cabeça... Não consigo por a dar

--- TP2/reader/LSXscene.js
+++ TP2/reader/LSXscene.js
@@ -40,9 +40,8 @@ LSXscene.prototype.init = function (application) {
 	//Test Block
 	this.TEST_Texture = new CGFtexture(this, "texture/High Altitude.JPG");
 	this.TEST_Terrain = new Terrain(this, 200);
-	this.TEST_Shader = new CGFshader(this.gl, "shaders/flatred.vert", "shaders/flatred.frag")
-	this.TEST_Shader.setUniformsValues({normScale: 50.0});
-	
+	this.TEST_Shader = new CGFshader(this.gl, "shaders/flat.vert", "shaders/red.frag")
+	this.TEST_Shader.setUniformsValues({uSampler: 1}); //O valor um iplica que a textura a usar deve fazer bind(1)
 	
 };
 
@@ -141,7 +140,7 @@ LSXscene.prototype.display = function () {
 		var change = mat4.create(); 
 		this.transformMatrix_m4(change, "translation", 0, -5, 0);
 		this.multMatrix(change);
-		this.TEST_Texture.bind();
+		this.TEST_Texture.bind(1);
 		this.TEST_Terrain.display();
 		this.setActiveShader(this.defaultShader);
 		
